92a1733e67e88b30ccc6d3740a27e6503d855792
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-06-28 22:49:29 -0800

    Create test-repo9.py

---
+++ test-repo9.py
@@ -0,0 +1,8 @@
+# example of using while loop in python
+# example of using f string in python
+x = 1
+
+# to run the integer numbers vertical from 1 to 5
+while x <= 5:
+    print(f'output of {x}')     # using f string here
+    x = x + 1
